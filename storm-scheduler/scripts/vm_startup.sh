bfbefbc465bb6c6f9a60296c0c67574e96b2505b
Théo HARDY <91427785+TheHardy04@users.noreply.github.com>
2026-02-17 12:38:40 +0100

    feat: Update vm_startup.sh to create logs directory and simplify permissions

--- storm-scheduler/scripts/vm_startup.sh
+++ storm-scheduler/scripts/vm_startup.sh
@@ -18,10 +18,10 @@ wget https://downloads.apache.org/storm/apache-storm-$STORM_VER/apache-storm-$ST
 tar -zxf apache-storm-$STORM_VER.tar.gz || { echo "Error: Failed to extract Apache Storm archive apache-storm-$STORM_VER.tar.gz." >&2; exit 1; }
 mv apache-storm-$STORM_VER /usr/local/storm
 
+# --- 4. SET PERMISSIONS AND PATHS ---
 # Give the right to everyone to read write and execute the storm directory (for simplicity in this VM setup)
 chmod -R 777 /usr/local/storm
-chmod -R 777 /usr/local/storm/conf
-chmod -R 777 /usr/local/storm/logs
+mkdir -p /usr/local/storm/logs
 
 # Add binaries to PATH
 ln -s /usr/local/storm/bin/storm /usr/bin/storm
